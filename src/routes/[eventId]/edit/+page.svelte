82fe874419850c96c1c206327e5428fbf731c974
Casey Plummer <casey.plummer@live.com>
2025-02-18 06:04:34 -0600

    Edit existing events. Using the same form for adding and editing event.

--- src/routes/[eventId]/edit/+page.svelte
+++ src/routes/[eventId]/edit/+page.svelte
@@ -1,4 +1,18 @@
 <script lang="ts">
+    import EventForm from '$lib/components/EventForm.svelte';
+    import type { ActionData, PageData } from './$types';
+
+    interface Props {
+        data: PageData, 
+        form: ActionData
+    }    
+    let {data, form}: Props = $props();
 </script>
 
-<p>Edit page works</p>
+{#await data.streamed.event}
+    <p>Loading...</p>
+{:then event}
+{#if event}
+    <EventForm {form} {event} />
+{/if}
+{/await}
