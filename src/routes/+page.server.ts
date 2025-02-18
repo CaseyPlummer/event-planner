a06eb1b11aa3818782aa0e8d279c3dc9857da6db
Casey Plummer <casey.plummer@live.com>
2025-02-18 07:39:25 -0600

    Delete existing event

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
-import { fetchAllEvents } from '$lib/server/remote-events';
-import type { PageServerLoad } from './$types';
+import { fetchAllEvents, deleteEventById } from "$lib/server/remote-events";
+import type { Actions, PageServerLoad } from "./$types";
+import { error, redirect } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async () => {    
     return {
@@ -8,3 +9,23 @@ export const load: PageServerLoad = async () => {
 		}        
     }
 };
+
+export const actions: Actions = {
+    delete: async ({ request}) => {
+        const formdata = await request.formData();
+        const id = formdata.get('id')?.toString()
+        const eventId = id ? parseInt(id) : undefined;
+        
+        if (!eventId) {
+            error(400, 'The event ID is required.');
+        }                   
+        const success = await deleteEventById(eventId);
+        
+        if (success) {
+            redirect(303, `/`);
+        }
+        else {
+            error(400, `The event with ID ${eventId} could not be deleted.`);
+        }        
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { enhance } from "$app/forms";
 	import type { PageData } from "./$types";
 
     let {data}: {data: PageData} = $props();
+
+    let deletingId : number | undefined = $state(undefined);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -17,9 +20,45 @@
             <h2 class="text-lg font-bold">{event.id}: {event.title}</h2>
             <p>{event.description}</p>
             <p>{event.date}</p>
-            <p><a class="btn" href="/{event.id}/edit" role="button">Edit Event</a></p>
+            <p class="inline-flex items-center" >
+                <a class="btn {deletingId ? 'disabled' : ''}" href="{deletingId ? '' : '/' + event.id + '/edit'}" role="{deletingId ? 'link' : 'button'}" aria-disabled={!!deletingId}>Edit Event</a>
+
+                <form method="POST" action="?/delete" class="inline-flex ml-2" 
+                use:enhance={() => {
+                    
+                    deletingId = event.id;            
+                    return async ({ update }) => {
+                        await update();
+                        deletingId = undefined;
+                    };
+                }}>
+                
+                <input type="text" id="id" name="id" class="hidden" value={event?.id}>               
+            
+                <button type="submit" class="btn inline-flex items-center" disabled={!!deletingId}>
+                    {#if deletingId === event.id}
+                        <svg class="animate-spin h-5 w-5 mr-2" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/>
+                            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 0 1 8-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 0 1 4 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
+                        </svg>
+                        Deleting...
+                    {:else}
+                        Delete Event
+                    {/if}
+                </button>
+            </form>
+
+
         </div>
     {/each}
 {/await}
 
-<a class="btn" href="/add" role="button">Add Event</a>
+<a class="btn mt-2" href="/add" role="button">Add Event</a>
+
+<style>
+    .btn.disabled {
+    @apply cursor-not-allowed;
+    } 
+</style>
+
+
