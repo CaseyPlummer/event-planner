41fd3310b54445fc9ac50f7ff56883f58db9d4ad
Casey Plummer <casey.plummer@live.com>
2025-02-19 07:13:37 -0600

    Very basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,55 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="app">
+	<!-- <Header /> -->
+
+	<div class="app-columns">
+		<main class="">
+			{@render children?.()}
+		</main>
+
+		<div class="sidebar-menu">
+			<!-- <Menu /> -->
+		</div>
+
+		<div class="sidebar-lists">
+			<!-- <Lists /> -->
+		</div>
+	</div>
+
+	<!-- <Footer /> -->
+</div>
+
+<style lang="scss">
+	.app {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+
+	.app-columns {
+		display: flex;
+		flex-direction: row;
+		flex: 1;
+		z-index: 1;
+		overflow: auto;
+	}
+
+	main {
+		overflow: auto;
+		padding: 2rem;
+	}
+
+	.sidebar-menu {
+		order: -1; // Main content appears first in html
+		z-index: 2;
+		display: none; // Not using yet
+	}
+
+	.sidebar-lists {
+		order: -1; // Main content appears first in html
+		z-index: 1;
+		display: none; // Not using yet
+	}
+</style>
